26202993035d4583ccca7af98e22ca850a9d4a9f
Raisa Shankhadev <np01cp4s230056@islingtoncollege.edu.np>
2025-01-16 11:41:35 +0545

    Database Models 💾

---
+++ Models/Filter.cs
@@ -0,0 +1,13 @@
+namespace BlazorPay.Models
+{
+    public class Filter
+    {
+        public string? Code { get; set; }
+        public string? Name { get; set; }
+
+        public override string ToString()
+        {
+            return Name ?? "Unknown Currency"; 
+        }
+    }
+}
